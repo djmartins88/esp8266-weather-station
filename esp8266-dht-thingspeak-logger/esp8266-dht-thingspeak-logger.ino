239a144c1d3a9c8356f6963db86f7e700241b9c8
David <djmartins.dev@gmail.com>
2024-11-25 08:46:03 +0000

    Fan and Water Atomizer on and off logic, also sharing status to thingspeak

--- esp8266-dht-thingspeak-logger/esp8266-dht-thingspeak-logger.ino
+++ esp8266-dht-thingspeak-logger/esp8266-dht-thingspeak-logger.ino
@@ -28,6 +28,12 @@ https://github.com/adafruit/DHT-sensor-library/blob/master/examples/DHT_Unified_
 #define SCK_PIN D4
 #define SDA_PIN D3
 
+// 12v fan controlled by relay - relay pin
+#define FAN_RELAY_PIN  D8  // The ESP8266 pin connected to the IN pin of relay
+
+// Water atomizer controlled by relay - relay pin
+#define ATOMIZER_RELAY_PIN D7 // TODO
+
 const char* SSID = "NAUGHTY";
 const char* PASSWORD = "VERYGOODBOY"; 
 const char* THINGSPEAK_API_KEY = "DONTFOMOONCRYPTO";
@@ -51,10 +57,26 @@ sensors_event_t event;
 const int UPDATE_INTERVAL_SECONDS = 600;
 
 // Read sensors every 10 seconds
-const int READ_INTERVAL_SECONDS = 10;
+const int READ_INTERVAL_SECONDS = 60;
+
+// Turn on Fan when temperature is above this value
+const int TEMPERATURE_TRESHOLD = 15;
+
+// Turn on water atomizer when humidity is bellow this value
+const int HUMIDITY_TRESHOLD = 80;
+
+// Control atomizer to work in x minutes intervals
+const int ATOMIZER_REST_INTERVAL_SECONDS = 3 * 60;
 
 long READ_TIME = 0;
 long UPDATE_TIME = 0;
+
+long ATOMIZER_TIME_START = -360000; // so automizer can start at beggining if needed
+
+bool FAN_WORKING = false;
+bool ATOM_WORKING = false;
+
+long TIME = 0;
 
 int humidity = 0;
 int temperature = 0;
@@ -96,19 +118,24 @@ void setup() {
   display.flipScreenVertically();
 
   // Wifi setup
-  Serial.print("Connecting to ");
-  Serial.println(SSID);
+  Serial.print("Connecting to "); Serial.print(SSID); Serial.print(" ");
   WiFi.begin(SSID, PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.print("   WiFi connected: ");
-  Serial.println(WiFi.localIP());
+  Serial.print(" connected: "); Serial.println(WiFi.localIP());
+
+  // 12v fan relay setup
+  pinMode(FAN_RELAY_PIN, OUTPUT);
+
+  // Water atomizer setup
+  pinMode(ATOMIZER_RELAY_PIN, OUTPUT);
 
   Serial.println("- - - - -  SETUP COMPLETE  - - - - -");
 }
 
+// read light value from light sensor
 void readLight() {
 
   Serial.print("Light = ");
@@ -133,6 +160,7 @@ void readLight() {
   Serial.println(light);
 }
 
+// read pressure from Atmosphere sensor - not being used
 void readPressure(){
 
   if (ATMOSPHERE_ON) {
@@ -149,6 +177,7 @@ void readPressure(){
   }
 }
 
+// read temperature from Atmosphere sensor
 void readTemperature(){
 
   if (ATMOSPHERE_ON) {
@@ -161,16 +190,19 @@ void readTemperature(){
 
 }
 
+// Read humidity from DHT sensor
 void readHumidity() {
 
+  Serial.print("Humidity = ");
   dht.humidity().getEvent(&event);
   humidity = event.relative_humidity;
-  Serial.print("Humidity = "); Serial.println(humidity);
+  Serial.print(humidity); Serial.println(" %");
 
   // if u want to use for temp: dht.temperature().getEvent(&event);
 }
 
-void updateThingsSpeak(int h, int t, int l, int a) {
+// update latest sensor readings to ThingSpeak
+void updateThingsSpeak() {
 
   // Serial.print("connecting to "); Serial.println(host);
 
@@ -185,14 +217,12 @@ void updateThingsSpeak(int h, int t, int l, int a) {
   // We now create a URI for the request
   String url = "/update?api_key=";
   url += THINGSPEAK_API_KEY;
-  url += "&field1=";
-  url += t;
-  url += "&field2=";
-  url += h;
-  url += "&field3=";
-  url += l;
-  url += "&field4=";
-  url += a;
+  url += "&field1="; url += temperature;
+  url += "&field2="; url += humidity;
+  url += "&field3="; url += light;
+  url += "&field4="; url += pressure;
+  url += "&field5="; url += FAN_WORKING;
+  url += "&field6="; url += ATOM_WORKING;
 
   // Serial.print("Requesting URL: "); Serial.println(url);
 
@@ -212,6 +242,7 @@ void updateThingsSpeak(int h, int t, int l, int a) {
   Serial.println();
 }
 
+// Simply write a message on the display
 void displayMessage(String message) {
   display.clear();
   display.setFont(ArialMT_Plain_10);
@@ -220,37 +251,100 @@ void displayMessage(String message) {
   display.display();
 }
 
-void displayWeatherStationInfo() {
+// print sensor data to serial
+void printWeatherStationSensorData() {
   display.clear();
   display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.drawStringMaxWidth(0, 0, 128, "Humidity: " + String(humidity) + " %");
   display.drawStringMaxWidth(0, 12, 128, "Temperature: " + String(temperature) + " ºC");
   display.drawStringMaxWidth(0, 24, 128, "Light: " + String(light));
+  display.drawStringMaxWidth(0, 48, 128, "Fan: " + String(FAN_WORKING ? "On" : "Off"));
+  display.drawStringMaxWidth(64, 48, 128, "Atom: " + String(ATOM_WORKING ? "On" : "Off"));
   display.display();
+}
+
+void checkAndAdjustTemperature() {
+
+  // check temperature is above treshold
+  if (temperature > TEMPERATURE_TRESHOLD && !FAN_WORKING) {
+    digitalWrite(FAN_RELAY_PIN, HIGH); // turn on fan via relay
+    FAN_WORKING = true;
+
+    Serial.println("Staring Fan - " + String(temperature) + " ºC temperature is above treshold (" + String(TEMPERATURE_TRESHOLD) + ") :: " + String(millis()));
+  } else if (temperature < TEMPERATURE_TRESHOLD && FAN_WORKING) {
+    digitalWrite(FAN_RELAY_PIN, LOW); // turn off fan via relay
+    FAN_WORKING = false;
+
+    Serial.println("Stopping Fan - " + String(temperature) + " ºC temperature is bellow or equal treshold (" + String(TEMPERATURE_TRESHOLD) + ") :: " + String(millis()));
+  }
+
+}
+
+void checkAndAdjustHumidity() {
+
+  // check humidity is bellow treshold
+  if (humidity < HUMIDITY_TRESHOLD) {
+
+    // atomizer is supposed to be working enforce he works X time, turns off, then starts again, until the reading is bellow treshold
+    if (TIME - ATOMIZER_TIME_START > 2 * ATOMIZER_REST_INTERVAL_SECONDS * 1000) {
+      digitalWrite(ATOMIZER_RELAY_PIN, HIGH); // turn on water atomizer
+      ATOMIZER_TIME_START = TIME;
+      ATOM_WORKING = true;
+
+      Serial.println("Starting atomizer - " + String(humidity) + " % humidity is bellow treshold (" + String(HUMIDITY_TRESHOLD) + ") :: " + String(TIME));
+    } 
+    // atomizer is supposed to be working but the first X seconds have passed, turn off atomizer
+    else if (TIME - ATOMIZER_TIME_START > ATOMIZER_REST_INTERVAL_SECONDS * 1000 && ATOM_WORKING) { 
+        
+        digitalWrite(ATOMIZER_RELAY_PIN, LOW); // turn off water atomizer
+        ATOM_WORKING = false;
+        
+        Serial.println("Making a break on Atomizer - " + String(humidity) + " % humidity is bellow treshold (" + String(HUMIDITY_TRESHOLD) + ") :: " + String(TIME));
+    }
+
+  } else if (ATOM_WORKING) {
+
+    digitalWrite(ATOMIZER_RELAY_PIN, LOW); // turn off water atomizer
+    ATOM_WORKING = false;
+
+    Serial.println("Stopping Atomizer - " + String(humidity) + " % humidity is above or equal treshold (" + String(HUMIDITY_TRESHOLD) + ") :: " + String(TIME));
+  }
+
+}
+
+// check if sensors are above treshold and act on it - play God.
+void adjustWeather() {
+  checkAndAdjustTemperature();
+  checkAndAdjustHumidity();
 }
 
 void loop() {
 
-  // read values from the sensor
-  if (millis() - READ_TIME > READ_INTERVAL_SECONDS * 1000) {
+  TIME = millis();
+
+  // read values from sensors
+  if (TIME - READ_TIME > READ_INTERVAL_SECONDS * 1000) {
 
     readTemperature();
     readLight();
     //readPressure();
     readHumidity();
 
-    READ_TIME = millis();
+    READ_TIME = TIME;
+
+    // play God.
+    adjustWeather();
   }
 
   // update ThingSpeak
-  if (millis() - UPDATE_TIME > UPDATE_INTERVAL_SECONDS * 1000) {
-    updateThingsSpeak(humidity, temperature, light, pressure);
+  if (TIME - UPDATE_TIME > UPDATE_INTERVAL_SECONDS * 1000) {
+    updateThingsSpeak();
 
-    UPDATE_TIME = millis();
+    UPDATE_TIME = TIME;
   }
 
-  displayWeatherStationInfo();
+  printWeatherStationSensorData();
 
   // Go back to sleep. If your sensor is battery powered you might
   delay(1000 * READ_INTERVAL_SECONDS);
